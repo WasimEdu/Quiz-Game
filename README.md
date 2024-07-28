05b0186846907a6a75e542660c20d67d400d5b40
WasimEdu <WASIM.20231CSG0054@presidencyuniversity.in>
2024-07-28 19:32:24 +0530

    finished

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ This is a very simple Quiz Game
 <br>
 Created in Python
 <br>
-This program will also tell user it's score (How many they have answered and also give them % of their answer). In this program there will be only 5 question answers. Fell free to as many as you want.!
+This program will also tell user it's score (How many they have answered and also give them % of their answer). In this program there will be only 4 simple question answers. Fell free to as many as you want.!
--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,53 @@
 # created by: Wasim
+
+print("Welcome to the Quiz Game!")
+
+answer = input("Do you wanna play this Game (yes/no): ").lower()
+
+score = 0
+
+if answer != "yes":
+    quit()
+
+answer = input("What stand for RAM? ").lower()
+
+if answer == "random access memory":
+    score += 1
+    print("Correct!")
+
+else:
+    print("Incorrect!")
+
+
+answer = input("What stand for CPU? ").lower()
+
+if answer == "central processing unit":
+    score += 1
+    print("Correct!")
+
+else:
+    print("Incorrect!")
+
+
+answer = input("What stand for ROM? ").lower()
+
+if answer == "read only memory":
+    score += 1
+    print("Correct!")
+
+else:
+    print("Incorrect!")
+
+
+answer = input("What stand for GPU? ").lower()
+
+if answer == "graphics processing unit":
+    score += 1
+    print("Correct!")
+
+else:
+    print("Incorrect")
+
+
+print("You have given " + str(score) + " correct answer!")
+print("Your got " + str( ((score/4) *100) ) + "%.")
